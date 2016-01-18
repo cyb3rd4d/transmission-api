ee471f3f6a6fff5a39a4e37c1d720ce30e472141
Martial Saunois <saunois.martial@gmail.com>
2016-01-18 22:50:53 +0100

    Update the "const" keywords to lower case

--- src/Argument/Session/Accessor.php
+++ src/Argument/Session/Accessor.php
@@ -7,207 +7,207 @@ interface Accessor
     /**
      * Value type: number
      */
-    CONST ALT_SPEED_DOWN = 'alt-speed-down';
+    const ALT_SPEED_DOWN = 'alt-speed-down';
 
     /**
      * Value type: boolean
      */
-    CONST ALT_SPEED_ENABLED = 'alt-speed-enabled';
+    const ALT_SPEED_ENABLED = 'alt-speed-enabled';
 
     /**
      * Value type: number
      */
-    CONST ALT_SPEED_TIME_BEGIN = 'alt-speed-time-begin';
+    const ALT_SPEED_TIME_BEGIN = 'alt-speed-time-begin';
 
     /**
      * Value type: boolean
      */
-    CONST ALT_SPEED_TIME_ENABLED = 'alt-speed-time-enabled';
+    const ALT_SPEED_TIME_ENABLED = 'alt-speed-time-enabled';
 
     /**
      * Value type: number
      */
-    CONST ALT_SPEED_TIME_END = 'alt-speed-time-end';
+    const ALT_SPEED_TIME_END = 'alt-speed-time-end';
 
     /**
      * Value type: number
      */
-    CONST ALT_SPEED_TIME_DAY = 'alt-speed-time-day';
+    const ALT_SPEED_TIME_DAY = 'alt-speed-time-day';
 
     /**
      * Value type: number
      */
-    CONST ALT_SPEED_UP = 'alt-speed-up';
+    const ALT_SPEED_UP = 'alt-speed-up';
 
     /**
      * Value type: string
      */
-    CONST BLOCKLIST_URL = 'blocklist-url';
+    const BLOCKLIST_URL = 'blocklist-url';
 
     /**
      * Value type: boolean
      */
-    CONST BLOCKLIST_ENABLED = 'blocklist-enabled';
+    const BLOCKLIST_ENABLED = 'blocklist-enabled';
 
     /**
      * Value type: number
      */
-    CONST CACHE_SIZE_MB = 'cache-size-mb';
+    const CACHE_SIZE_MB = 'cache-size-mb';
 
     /**
      * Value type: string
      */
-    CONST DOWNLOAD_DIR = 'download-dir';
+    const DOWNLOAD_DIR = 'download-dir';
 
     /**
      * Value type: number
      */
-    CONST DOWNLOAD_QUEUE_SIZE = 'download-queue-size';
+    const DOWNLOAD_QUEUE_SIZE = 'download-queue-size';
 
     /**
      * Value type: boolean
      */
-    CONST DOWNLOAD_QUEUE_ENABLED = 'download-queue-enabled';
+    const DOWNLOAD_QUEUE_ENABLED = 'download-queue-enabled';
 
     /**
      * Value type: boolean
      */
-    CONST DHT_ENABLED = 'dht-enabled';
+    const DHT_ENABLED = 'dht-enabled';
 
     /**
      * Value type: string
      */
-    CONST ENCRYPTION = 'encryption';
+    const ENCRYPTION = 'encryption';
 
     /**
      * Value type: number
      */
-    CONST IDLE_SEEDING_LIMIT = 'idle-seeding-limit';
+    const IDLE_SEEDING_LIMIT = 'idle-seeding-limit';
 
     /**
      * Value type: boolean
      */
-    CONST IDLE_SEEDING_LIMIT_ENABLED = 'idle-seeding-limit-enabled';
+    const IDLE_SEEDING_LIMIT_ENABLED = 'idle-seeding-limit-enabled';
 
     /**
      * Value type: string
      */
-    CONST INCOMPLETE_DIR = 'incomplete-dir';
+    const INCOMPLETE_DIR = 'incomplete-dir';
 
     /**
      * Value type: boolean
      */
-    CONST INCOMPLETE_DIR_ENABLED = 'incomplete-dir-enabled';
+    const INCOMPLETE_DIR_ENABLED = 'incomplete-dir-enabled';
 
     /**
      * Value type: boolean
      */
-    CONST LPD_ENABLED = 'lpd-enabled';
+    const LPD_ENABLED = 'lpd-enabled';
 
     /**
      * Value type: number
      */
-    CONST PEER_LIMIT_GLOBAL = 'peer-limit-global';
+    const PEER_LIMIT_GLOBAL = 'peer-limit-global';
 
     /**
      * Value type: number
      */
-    CONST PEER_LIMIT_PER_TORRENT = 'peer-limit-per-torrent';
+    const PEER_LIMIT_PER_TORRENT = 'peer-limit-per-torrent';
 
     /**
      * Value type: boolean
      */
-    CONST PEX_ENABLED = 'pex-enabled';
+    const PEX_ENABLED = 'pex-enabled';
 
     /**
      * Value type: number
      */
-    CONST PEER_PORT = 'peer-port';
+    const PEER_PORT = 'peer-port';
 
     /**
      * Value type: boolean
      */
-    CONST PEER_PORT_RANDOM_ON_START = 'peer-port-random-on-start';
+    const PEER_PORT_RANDOM_ON_START = 'peer-port-random-on-start';
 
     /**
      * Value type: boolean
      */
-    CONST PORT_FORWARDING_ENABLED = 'port-forwarding-enabled';
+    const PORT_FORWARDING_ENABLED = 'port-forwarding-enabled';
 
     /**
      * Value type: boolean
      */
-    CONST QUEUE_STALLED_ENABLED = 'queue-stalled-enabled';
+    const QUEUE_STALLED_ENABLED = 'queue-stalled-enabled';
 
     /**
      * Value type: number
      */
-    CONST QUEUE_STALLED_MINUTES = 'queue-stalled-minutes';
+    const QUEUE_STALLED_MINUTES = 'queue-stalled-minutes';
 
     /**
      * Value type: boolean
      */
-    CONST RENAMED_PARTIAL_FILES = 'rename-partial-files';
+    const RENAMED_PARTIAL_FILES = 'rename-partial-files';
 
     /**
      * Value type: string
      */
-    CONST SCRIPT_TORRENT_DONE_FILENAME = 'script-torrent-done-filename';
+    const SCRIPT_TORRENT_DONE_FILENAME = 'script-torrent-done-filename';
 
     /**
      * Value type: boolean
      */
-    CONST SCRIPT_TORRENT_DONE_ENABLED = 'script-torrent-done-enabled';
+    const SCRIPT_TORRENT_DONE_ENABLED = 'script-torrent-done-enabled';
 
     /**
      * Value type: double
      */
-    CONST SEED_RATIO_LIMIT = 'seedRatioLimit';
+    const SEED_RATIO_LIMIT = 'seedRatioLimit';
 
     /**
      * Value type: boolean
      */
-    CONST SEED_RATIO_LIMITED = 'seedRatioLimited';
+    const SEED_RATIO_LIMITED = 'seedRatioLimited';
 
     /**
      * Value type: number
      */
-    CONST SEED_QUEUE_SIZE = 'seed-queue-size';
+    const SEED_QUEUE_SIZE = 'seed-queue-size';
 
     /**
      * Value type: boolean
      */
-    CONST SEED_QUEUE_ENABLED = 'seed-queue-enabled';
+    const SEED_QUEUE_ENABLED = 'seed-queue-enabled';
 
     /**
      * Value type: number
      */
-    CONST SPEED_LIMIT_DOWN = 'speed-limit-down';
+    const SPEED_LIMIT_DOWN = 'speed-limit-down';
 
     /**
      * Value type: boolean
      */
-    CONST SPEED_LIMIT_DOWN_ENABLED = 'speed-limit-down-enabled';
+    const SPEED_LIMIT_DOWN_ENABLED = 'speed-limit-down-enabled';
 
     /**
      * Value type: number
      */
-    CONST SPEED_LIMIT_UP = 'speed-limit-up';
+    const SPEED_LIMIT_UP = 'speed-limit-up';
 
     /**
      * Value type: boolean
      */
-    CONST SPEED_LIMIT_UP_ENABLED = 'speed-limit-up-enabled';
+    const SPEED_LIMIT_UP_ENABLED = 'speed-limit-up-enabled';
 
     /**
      * Value type: boolean
      */
-    CONST START_ADDED_TORRENTS = 'start-added-torrents';
+    const START_ADDED_TORRENTS = 'start-added-torrents';
 
     /**
      * Value type: boolean
      */
-    CONST TRASH_ORIGINAL_TORRENT_FILES = 'trash-original-torrent-files';
+    const TRASH_ORIGINAL_TORRENT_FILES = 'trash-original-torrent-files';
 
     /**
      * Value type: array
@@ -219,10 +219,10 @@ interface Accessor
      * - memory-units: array - 4 strings: KB/s, MB/s, GB/s, TB/s
      * - memory-bytes: number - number of bytes in a KB (1000 for kB; 1024 for KiB)
      */
-    CONST UNITS = 'units';
+    const UNITS = 'units';
 
     /**
      * Value type: boolean
      */
-    CONST UTP_ENABLED = 'utp-enabled';
+    const UTP_ENABLED = 'utp-enabled';
 }
--- src/Argument/Session/Get.php
+++ src/Argument/Session/Get.php
@@ -7,25 +7,25 @@ interface Get extends Accessor
     /**
      * Value type: number
      */
-    CONST BLOCKLIST_SIZE = 'blocklist-size';
+    const BLOCKLIST_SIZE = 'blocklist-size';
 
     /**
      * Value type: string
      */
-    CONST CONFIG_DIR = 'config-dir';
+    const CONFIG_DIR = 'config-dir';
 
     /**
      * Value type: number
      */
-    CONST RPC_VERSION = 'rpc-version';
+    const RPC_VERSION = 'rpc-version';
 
     /**
      * Value type: number
      */
-    CONST RPC_VERSION_MINIMUM = 'rpc-version-minimum';
+    const RPC_VERSION_MINIMUM = 'rpc-version-minimum';
 
     /**
      * Value type: string
      */
-    CONST VERSION = 'version';
+    const VERSION = 'version';
 }
